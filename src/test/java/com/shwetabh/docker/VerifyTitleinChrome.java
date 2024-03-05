4d376ce085296cff26692ab9eaa6cffa0a41f574
Gaurishankarwadikar <153654046+Gaurishankarwadikar@users.noreply.github.com>
2024-03-05 10:21:22 +0530

    Update VerifyTitleinChrome.java

--- src/test/java/com/shwetabh/docker/VerifyTitleinChrome.java
+++ src/test/java/com/shwetabh/docker/VerifyTitleinChrome.java
@@ -1,12 +1,10 @@
 package com.shwetabh.docker;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.UnexpectedAlertBehaviour;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.BrowserType;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
@@ -15,30 +13,42 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 public class VerifyTitleinChrome {
-	static WebDriver driver;
-	@BeforeClass
-	public void setup() throws MalformedURLException
-	{
-		System.out.println("Running test in docker ");
-		WebDriverManager.chromedriver().setup();
-		driver= new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-	}
-	@Test
-	public void aajtakSearch() throws InterruptedException
-	{	
-		driver.navigate().to("https://www.aajtak.in/");
-		System.out.println("Title of page is "+driver.getTitle());
-		Thread.sleep(2000);
-	}
-	@AfterClass
-	public void teardown() throws InterruptedException
-	{
-		driver.quit();
-		Thread.sleep(2000);
-	}
+    static WebDriver driver;
+
+    @BeforeClass
+    public void setup() throws MalformedURLException {
+        System.out.println("Running test in Docker");
+
+        // Use WebDriverManager to setup ChromeDriver
+        WebDriverManager.chromedriver().setup();
+
+        // Define capabilities for Chrome browser
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setBrowserName("chrome");
+
+        // Set up the URL for Selenium Grid hub
+        String gridUrl = "http://localhost:4444/wd/hub";
+
+        // Create a RemoteWebDriver instance
+        driver = new RemoteWebDriver(new URL(gridUrl), capabilities);
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void aajtakSearch() throws InterruptedException {
+        driver.navigate().to("https://www.aajtak.in/");
+        System.out.println("Title of page is " + driver.getTitle());
+        Thread.sleep(2000);
+    }
+
+    @AfterClass
+    public void teardown() throws InterruptedException {
+        if (driver != null) {
+            driver.quit();
+        }
+        Thread.sleep(2000);
+    }
 }
